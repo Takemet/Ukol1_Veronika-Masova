9e7c4f30cc270bfadfefb44854415773010c475a
Veronika Mášová <masova.nika@seznam.cz>
2024-04-21 17:26:56 +0200

    první verze úkolu

---
+++ script.js
@@ -0,0 +1,6 @@
+document.body.innerHTML +="<h1>Níže naleznete výsledek převodu Vámi zadané teploty. Výsledek je zobrazen zaokrouhleně na celé číslo.</h1>"
+
+
+const degreeFahrenheit = prompt("Napište teplotu ve stupních Fahrenheita (pouze číslo) a stiskněte OK nebo enter na klávesnici");
+
+document.body.innerHTML += "<h2>" + degreeFahrenheit + "°F" + " " + " = " + Math.round((5 / 9 * (degreeFahrenheit - 32))) + "°C" + "</h2>"
